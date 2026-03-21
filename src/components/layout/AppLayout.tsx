e19c05f11e611b9def6f6ed94a54fd467637da84
Dave <david.oludepo@gmail.com>
2026-03-21 13:00:16 +0100

    style: add global page slide-up transition for all routes

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { Header } from "./Header";
@@ -12,14 +13,17 @@ interface AppLayoutProps {
 
 export function AppLayout({ children }: AppLayoutProps) {
   const [aiOpen, setAiOpen] = useState(false);
+  const location = useLocation();
 
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="flex-1 flex flex-col min-w-0 transition-all duration-300 ease-in-out">
           <Header aiOpen={aiOpen} setAiOpen={setAiOpen} />
-          <main className="flex-1 overflow-y-auto h-[calc(100vh-4rem)]">
-            {children}
+          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-background/50 h-[calc(100vh-4rem)]">
+            <div key={location.pathname} className="animate-fade-in-up min-h-full w-full">
+              {children}
+            </div>
             <GlobalAttendancePopup />
             <CommandMenu />
           </main>
